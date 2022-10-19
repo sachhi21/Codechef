5f02e04f701a1ae15dec53e42748aaf233c7e741
Sachin Rampur <99388531+sachhi21@users.noreply.github.com>
2022-10-19 13:31:14 +0530

    Add files via upload

---
+++ Who is taller!.cpp
@@ -0,0 +1,19 @@
+//Who is taller!
+#include <iostream>
+using namespace std;
+
+int main() {
+	  int T;
+	  cin>>T;
+	  while(T--){
+	      int X,Y;
+	      cin>>X>>Y;
+	      if(X>Y){
+	          cout<<"A"<<endl;
+	          
+	      }else{
+	          cout<<"B"<<endl;
+	      }
+	  }
+	return 0;
+}
